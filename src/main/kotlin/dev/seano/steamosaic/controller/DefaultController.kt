37554cf4b1e5d30294eeac78ffd8f87fe0bae277
Sean O'Connor <so@seano.dev>
2025-10-24 18:38:07 -0600

    Add DefaultController and mosaic endpoint

---
+++ src/main/kotlin/dev/seano/steamosaic/controller/DefaultController.kt
@@ -0,0 +1,17 @@
+package dev.seano.steamosaic.controller
+
+import dev.seano.steamosaic.service.MosaicService
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@Controller
+class DefaultController(private val mosaicService: MosaicService) {
+    @GetMapping("/{steamId}", produces = [MediaType.IMAGE_PNG_VALUE])
+    fun mosaic(@PathVariable steamId: String): ResponseEntity<ByteArray?> {
+        val mosaic = mosaicService.buildMosaic(steamId)
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(mosaic)
+    }
+}
