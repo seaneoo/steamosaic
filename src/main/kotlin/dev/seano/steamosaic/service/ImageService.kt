867852b0854b2d3b1104c5f2db4c896fccbc43f3
Sean O'Connor <so@seano.dev>
2025-10-25 12:24:08 -0600

    Add image resizing functionality to ImageService

--- src/main/kotlin/dev/seano/steamosaic/service/ImageService.kt
+++ src/main/kotlin/dev/seano/steamosaic/service/ImageService.kt
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
+import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
 import javax.imageio.ImageIO
@@ -14,6 +15,25 @@ class ImageService {
     private val restClient = RestClient.create()
     private val maxBytes: Long = 100_000
     private val logger: Logger = LoggerFactory.getLogger(ImageService::class.java.name)
+
+    companion object {
+        const val DEFAULT_WIDTH = 460
+
+        const val DEFAULT_HEIGHT = 215
+    }
+
+    private fun resize(
+        image: BufferedImage,
+        width: Int = DEFAULT_WIDTH,
+        height: Int = DEFAULT_HEIGHT,
+    ): BufferedImage {
+        val temp = image.getScaledInstance(width, height, Image.SCALE_SMOOTH)
+        val resized = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+        val g2d = resized.createGraphics()
+        g2d.drawImage(temp, 0, 0, null)
+        g2d.dispose()
+        return resized
+    }
 
     fun fetchImageFromUrl(urlString: String): BufferedImage? {
         logger.info("Fetching image from URL: $urlString")
@@ -43,7 +63,14 @@ class ImageService {
                 null
             } else {
                 logger.info("Successfully fetched and decoded image of ${bytes.size} bytes.")
-                image
+                if (image.width != DEFAULT_WIDTH || image.height != DEFAULT_HEIGHT) {
+                    logger.info(
+                        "Resizing image from ${image.width}x${image.height} to ${DEFAULT_WIDTH}x${DEFAULT_HEIGHT}."
+                    )
+                    resize(image)
+                } else {
+                    image
+                }
             }
         }
     }
