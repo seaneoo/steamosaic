dd40fca3dc8ac678af83badf06f7cf43e22249c8
Sean O'Connor <so@seano.dev>
2025-10-24 18:28:01 -0600

    Refactor fetchSteamId endpoint to use PathVariable and return plain text response
    This commit also removes the fetchOwnedGames and fetchGameAssets endpoints.

--- src/main/kotlin/dev/seano/steamosaic/controller/ApiController.kt
+++ src/main/kotlin/dev/seano/steamosaic/controller/ApiController.kt
@@ -1,38 +1,19 @@
 package dev.seano.steamosaic.controller
 
-import dev.seano.steamosaic.api.OwnedGames
 import dev.seano.steamosaic.api.SteamApiService
-import dev.seano.steamosaic.api.SteamId
-import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
 class ApiController(private val steamApiService: SteamApiService) {
-    @GetMapping("/id")
-    fun fetchSteamId(@RequestParam name: String): ResponseEntity<SteamId?> {
-        val response = steamApiService.fetchSteamId(name)
-        return ResponseEntity.ok(response.response)
-    }
-
-    @GetMapping("/games")
-    fun fetchOwnedGames(@RequestParam id: String): ResponseEntity<OwnedGames?> {
-        val response = steamApiService.fetchOwnedGames(id)
-        return ResponseEntity.ok(response.response)
-    }
-
-    @GetMapping("/assets")
-    fun fetchGameAssets(@RequestParam id: String, httpResponse: HttpServletResponse) {
-        val response = steamApiService.fetchGameAssets(id)
-        val image =
-            steamApiService.getHeaderImageUrl(
-                response.appId.toString(),
-                response.assets.header.split("/").first(),
-            )
-        httpResponse.sendRedirect(image.toString())
+    @GetMapping("/steam-id/{name}", produces = [MediaType.TEXT_PLAIN_VALUE])
+    fun fetchSteamId(@PathVariable name: String): ResponseEntity<String?> {
+        val steamId = steamApiService.fetchSteamId(name).response.steamId
+        return ResponseEntity.ok().contentType(MediaType.TEXT_PLAIN).body(steamId)
     }
 }
